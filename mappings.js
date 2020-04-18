82e54248fd79e48b005f55c12c4e4df120597bcd
Corey Farrell <git@cfware.com>
2020-04-18 18:12:34 -0400

    Remove unused variable

--- mappings.js
+++ mappings.js
@@ -5,7 +5,7 @@ export class Mappings extends Map {
 		super();
 
 		const doFetch = process.env.DO_FETCH !== '0';
-		for (const [id, {to: child}] of node.edgesOut.entries()) {
+		for (const {to: child} of node.edgesOut.values()) {
 			const resolved = child.package.resolved || child.package._resolved;
 			const fullId = `${child.name}@${child.package.version}`;
 			this.set(child.name, inventory.findOrCreate(fullId, () => ({
