d0fb88a42c547e07cf19375f67fa989e9831aa2b
Corey Farrell <git@cfware.com>
2020-04-18 18:10:41 -0400

    Fix DO_FETCH, mention on readme

--- README.md
+++ README.md
@@ -6,3 +6,8 @@ Mapped Package Manager Experiment
 
 Enter the `demo` directory and run `node ../install.js`.  This will create a `node_modules`
 directory in the proposed layout.
+
+### Environment
+
+The `install.js` script responds to `process.env.DO_FETCH === '0'`.  Setting this will
+prevent download of actual packages, only generating `node_modules/mappings.json`.
--- mappings.js
+++ mappings.js
@@ -4,13 +4,15 @@ export class Mappings extends Map {
 	constructor(inventory, node) {
 		super();
 
+		const doFetch = process.env.DO_FETCH !== '0';
 		for (const [id, {to: child}] of node.edgesOut.entries()) {
+			const resolved = child.package.resolved || child.package._resolved;
 			const fullId = `${child.name}@${child.package.version}`;
 			this.set(child.name, inventory.findOrCreate(fullId, () => ({
 				fullId,
-				resolved: child.package.resolved || child.package._resolved,
+				resolved,
 				mappings: new Mappings(inventory, child),
-				tar: process.env.DO_FETCH === '1' ? fetch(resolved) : null
+				tar: doFetch ? fetch(resolved) : null
 			})));
 		}
 	}
